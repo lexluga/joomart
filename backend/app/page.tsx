7f6b12330554e6802f7cdb61b30d1d2c95e9db83
Lex Luga <lexvillia@gmail.com>
2025-08-26 16:24:02 +0300

    Rebuild and Publish Api Backend and Frontend Docker Images to GHRC #1

--- backend/app/page.tsx
+++ backend/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly (HMR Works) </li>
-          {/* test 1*/}
+          {/* Test */}
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
