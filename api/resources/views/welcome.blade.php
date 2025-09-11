e7bed9866a74449505946080cabfe5c11f60f9d3
Lex Luga <lexvillia@gmail.com>
2025-09-11 17:56:10 +0300

    Modify & Test our docker images before pushing to staging #6

--- api/resources/views/welcome.blade.php
+++ api/resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    {{-- Test 1 --}}
+    {{-- Test  --}}
 
     <title> Joomart API </title>
     <meta name="description" content="Joomart API - The open and free API for all things Joomart.">
